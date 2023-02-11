4005d7fd2c3dd8501932a7887c90a739c3a21a09
Shubham Chauhan <submchauhan007@gmail.com>
2023-02-11 08:41:18 +0530

    new changes

--- src/main/java/com/example/todo/model/Todo.java
+++ src/main/java/com/example/todo/model/Todo.java
@@ -1,5 +1,9 @@
 package com.example.todo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,8 +11,11 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity //to connect with database
 
 public class Todo {
+    @Id // use tp make primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // use to auto increment
     private int id;
     private String title;
     private boolean status;
